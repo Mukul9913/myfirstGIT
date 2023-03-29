3aae665f1fbfd5c381b76bd63f070dd1e47faf6d
Mukul <mukuldixit931@gmail.com>
2023-03-29 17:26:05 +0530

    new

---
+++ git-first/oops1.js
@@ -0,0 +1,11 @@
+class Addition {
+    constructor(a, b) {
+        this.a = a;
+        this.b = b;
+    }
+    add() {
+        console.log("Addition :" + (this.a + this.b));
+    }
+}
+let obj = new Addition(29, 33);
+obj.add();
